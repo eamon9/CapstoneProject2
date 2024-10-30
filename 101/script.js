09b30b0fe88f770318e5a5f363b0676a31da7bf5
eamon9 <eamon9@icloud.com>
2024-10-30 16:09:19 +0200

    remove folder 101

--- 101/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSection(sectionId) {
-  const section = document.getElementById(sectionId);
-  section.classList.toggle("hidden");
-  const header = section.previousElementSibling;
-  header.textContent = section.classList.contains("hidden")
-    ? header.textContent.replace("-", "+")
-    : header.textContent.replace("+", "-");
-}
